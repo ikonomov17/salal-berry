2bf7fd424a9c7cc03813950306b9f30c7f46e769
daniel-slavov <danielslavov@Daniels-MacBook-Air.local>
2017-01-07 12:37:03 +0200

    InvalidChoiceException
    InvalidChoiceException added.

---
+++ salal-berry/Hero Classes version a/Main Program/InvalidChoiceException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Main_Program
+{
+	public class InvalidChoiceException : ApplicationException
+	{
+		private readonly string fileName;
+		private readonly int lineNumber;
+
+		public InvalidChoiceException(string message, string filename, int linenumber, Exception exception) : base(message, exception)
+		{
+			this.fileName = filename;
+			this.lineNumber = linenumber;
+		}
+
+		public string FileName { get; }
+
+		public int LineNumber { get; }
+
+		public override string Message
+		{
+			get
+			{
+				return string.Format($"{base.Message} (file: {this.fileName})");
+			}
+		}
+	}
+}
