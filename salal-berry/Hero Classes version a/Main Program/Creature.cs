6f5448f53167856584af3fff44dd79b3dd969e01
roc07 <gk.vasil.stoychev@mail.bg>
2017-01-07 12:08:18 +0200

    Updated

--- salal-berry/Hero Classes version a/Main Program/Creature.cs
+++ salal-berry/Hero Classes version a/Main Program/Creature.cs
@@ -13,17 +13,12 @@ namespace Main_Program
 
         public Creature()
         {
-
-        }
-
-        public Creature(int health, int damage, int armor, int strength, int agility, int speed)
-        {
-            this.health = health;
-            this.damage = damage;
-            this.armor = armor;
-            this.strength = strength;
-            this.agility = agility;
-            this.speed = speed;
+            generateHealth();
+            generateDamage();
+            generateArmor();
+            generateStrength();
+            generateAgility();
+            generateSpeed();
         }
 
         public int Health
@@ -62,6 +57,47 @@ namespace Main_Program
             set { this.speed = value; }
         }
 
+        public int generateHealth()
+        {
+            Random setter = new Random();
+            this.health = setter.Next(130, 171);
+            return health;
+        }
+
+        public int generateDamage()
+        {
+            Random setter = new Random();
+            this.damage = setter.Next(3, 7);
+            return damage;
+        }
+
+        public int generateArmor()
+        {
+            Random setter = new Random();
+            this.armor = setter.Next(0, 2);
+            return armor;
+        }
+
+        public int generateStrength()
+        {
+            Random setter = new Random();
+            this.strength = setter.Next(5, 10);
+            return strength;
+        }
+
+        public int generateAgility()
+        {
+            Random setter = new Random();
+            this.agility = setter.Next(5, 10);
+            return agility;
+        }
+
+        public int generateSpeed()
+        {
+            Random setter = new Random();
+            this.speed = setter.Next(85, 115);
+            return speed;
+        }
 
     }
 }
