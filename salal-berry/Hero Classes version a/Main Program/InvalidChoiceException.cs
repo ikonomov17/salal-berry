dc8bdf8dd70565509f730520793521603c009fce
daniel-slavov <danielslavov@Daniels-MacBook-Air.local>
2017-01-08 10:50:48 +0200

    Updated Custom Exception

--- salal-berry/Hero Classes version a/Main Program/InvalidChoiceException.cs
+++ salal-berry/Hero Classes version a/Main Program/InvalidChoiceException.cs
@@ -4,25 +4,12 @@ namespace Main_Program
 {
 	public class InvalidChoiceException : ApplicationException
 	{
-		private readonly string fileName;
-		private readonly int lineNumber;
-
-		public InvalidChoiceException(string message, string filename, int linenumber, Exception exception) : base(message, exception)
+		public InvalidChoiceException()
 		{
-			this.fileName = filename;
-			this.lineNumber = linenumber;
 		}
 
-		public string FileName { get; }
-
-		public int LineNumber { get; }
-
-		public override string Message
+		public InvalidChoiceException(string message) : base(message)
 		{
-			get
-			{
-				return string.Format($"{base.Message} (file: {this.fileName})");
-			}
 		}
 	}
 }
