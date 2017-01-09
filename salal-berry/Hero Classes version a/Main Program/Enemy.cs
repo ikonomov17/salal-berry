d09f59924d8f3042a6acc2cc34c22c307978b505
PamirKalo <kbm2@mail.bg>
2017-01-09 18:41:21 +0200

    Add class Enemy.cs

---
+++ salal-berry/Hero Classes version a/Main Program/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Program
+{
+    class Enemy : Creature
+    {
+        // properties
+        public Difficulty Rank { get; internal set; }
+        public int ScoreReward { get; internal set; }
+
+        // constructors
+        public Enemy(Difficulty rank, int scoreReward)
+        {
+            this.Rank = rank;
+            this.ScoreReward = scoreReward;
+        }
+    }
+}
