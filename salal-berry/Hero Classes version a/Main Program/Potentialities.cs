f266778e53d228a376fca89cfe5f75bb6c862d63
PamirKalo <kbm2@mail.bg>
2017-01-09 18:50:55 +0200

    Add class enum Potentialities.cs

---
+++ salal-berry/Hero Classes version a/Main Program/Potentialities.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Main_Program
+{
+    public enum Potentialities
+    {
+        Barracks = 1, MagicTower = 8, Forrest = 2, YogaТeacher = 2, InvisibleTrap = 1, RiverOfLife = 10
+    }
+}
