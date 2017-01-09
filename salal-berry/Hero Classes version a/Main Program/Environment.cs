8df7808339fbc07824b69238a3032ee4d3ca3fc4
PamirKalo <kbm2@mail.bg>
2017-01-09 18:40:22 +0200

    Add class Environment.cs

---
+++ salal-berry/Hero Classes version a/Main Program/Environment.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Main_Program
+{
+    public static class Environment
+    {
+        // these methods influence of the creature's abilities 
+         static void GetArmor(Creature creature)
+        {
+            creature.Armor += (int)Potentialities.Barracks;
+        }
+
+          static void GetHealth(Creature creature)
+        {
+            creature.Health += (int)Potentialities.RiverOfLife;
+        }
+
+          static void DoDamage(Creature creature)
+        {
+            creature.Damage -= (int)Potentialities.InvisibleTrap;
+        }
+
+          static void GetAgility(Creature creature)
+        {
+            creature.Health += (int)Potentialities.YogaТeacher;
+        }
+
+          static void GetSpeed(Creature creature)
+        {
+            creature.Damage -= (int)Potentialities.MagicTower;
+        }
+
+        static void GetStrength(Creature creature)
+        {
+            creature.Health += (int)Potentialities.Forrest;
+        }
+    }
+}
+
