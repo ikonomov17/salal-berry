97f21fe4b5428f726f0a1be24488a1f5585dfcf6
Aleksandar Ikonomov <aleksandar.ikonomov@abv.bg>
2017-01-11 12:22:42 +0200

    Add Magic.cs class.

---
+++ salal-berry/Hero Classes version a/Main Program/Magic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Main_Program
+{
+    public static class Magic
+    {
+
+        //Since Creature's health is between 130 and 171, the heal is 
+        //10 % of Creature possible health.Invoke the method like this:
+        //test1.Heal();
+
+        public static int Heal(this Creature one)
+        {
+            Random rand = new Random();
+            int heal = rand.Next(13, 17);
+            return heal;
+        }
+
+        //Since Fireball is magic it ignores opponent's armor.
+        //Causes random damage between 5 and 10
+        //Creature one attacks creature two with fireball
+        //Invoke method :  test1.Fireball(test2);
+
+        public static int Fireball(this Creature one,Creature two)
+        {
+            Random rand = new Random();
+            int fireball = rand.Next(5, 10);
+            //TODO change Health access modifier
+            //two.Health -= fireball;
+            return fireball;
+        }
+    }
+}
